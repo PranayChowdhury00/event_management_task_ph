3c34a8805ede5fb7fff24d47342dc66a7fe67c00
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2025-06-29 18:04:30 +0600

    initial setup

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,12 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="">Hello world!</div>,
+  },
+]);
+export default router;
